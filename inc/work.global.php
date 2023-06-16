f92f3b5f4af4a083d544f7a378b86854cb17dd2f
Chris Watterston <chris@chriswatterston.com>
2023-06-16 18:10:27 +0100

    Replaced work images

--- inc/work.global.php
+++ inc/work.global.php
@@ -1,19 +1,19 @@
 <section class="<?= $globalPrefix; ?>-carousel-container _max-width__1640 _margin-bottom__default" id="latest-work">
     <div class="<?= $globalPrefix; ?>-grid-container -column__carousel -column-gap__default" id="__carousel-main">
         <div class="__slide __one">
-            <img src="/dist/images/work/digital-design-subscription-01.jpg" alt="Design subscriptions work by Drip Fed Design" class="__image" width="907px" height="554px" />
+            <img src="/dist/images/work/digital-design-subscription-01.jpg" alt="Design subscriptions work by Drip Fed Design" class="__image" width="900px" height="550px" />
         </div>
         <div class="__slide __two">
-            <img src="/dist/images/work/digital-design-subscription-02.jpg" alt="Design subscriptions work by Drip Fed Design" class="__image" width="907px" height="554px" />
+            <img src="/dist/images/work/digital-design-subscription-02.jpg" alt="Design subscriptions work by Drip Fed Design" class="__image" width="900px" height="550px" />
         </div>
         <div class="__slide __three">
-            <img src="/dist/images/work/digital-design-subscription-03.jpg" alt="Design subscriptions work by Drip Fed Design" class="__image" width="907px" height="554px" />
+            <img src="/dist/images/work/digital-design-subscription-03.jpg" alt="Design subscriptions work by Drip Fed Design" class="__image" width="900px" height="550px" />
         </div>
         <div class="__slide __four">
-            <img src="/dist/images/work/digital-design-subscription-04.jpg" alt="Design subscriptions work by Drip Fed Design" class="__image" width="907px" height="554px" />
+            <img src="/dist/images/work/digital-design-subscription-04.jpg" alt="Design subscriptions work by Drip Fed Design" class="__image" width="900px" height="550px" />
         </div>
         <div class="__slide __five">
-            <img src="/dist/images/work/digital-design-subscription-05.jpg" alt="Design subscriptions work by Drip Fed Design" class="__image" width="907px" height="554px" />
+            <img src="/dist/images/work/digital-design-subscription-05.jpg" alt="Design subscriptions work by Drip Fed Design" class="__image" width="900px" height="550px" />
         </div>
     </div>
 </section>
