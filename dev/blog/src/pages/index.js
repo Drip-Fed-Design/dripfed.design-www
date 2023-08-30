a49b901ba833b5c2101787effb03bbda17e2ed8e
Chris Watterston <chris@chriswatterston.com>
2023-08-30 19:00:24 +0100

    New blog post

--- dev/blog/src/pages/index.js
+++ dev/blog/src/pages/index.js
@@ -13,8 +13,8 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location}>
-        <Seo title="Blog" />
-        <Hero title="Blog" />
+        <Seo title="Blog & Work Updates" />
+        <Hero title="Blog & Work Updates" />
         <ArticlePreview posts={posts} />
       </Layout>
     );
