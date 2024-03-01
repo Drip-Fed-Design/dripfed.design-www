d4f280633b65d31ec0ba1a1700cea0475b8241e4
Chris Watterston <chris@chriswatterston.com>
2024-03-01 10:45:03 +0000

    Added links to portfolio on all header image

--- inc/work.global.php
+++ inc/work.global.php
@@ -1,10 +1,14 @@
 <section class="<?= $globalPrefix; ?>-carousel-container _max-width__1640 _padding-bottom__default" id="latest-work">
     <div class="<?= $globalPrefix; ?>-grid-container -column__carousel -column-gap__default" id="__carousel-main">
         <div class="__slide __one">
-            <img loading="lazy" src="/dist/images/work/digital-design-subscription-01.jpg" alt="Unlimited design subscriptions work by Drip Fed Design agency" class="__image" width="900px" height="550px" />
+            <a data-cabin-event="Portfolio / IMG-01" href="<?= $globalDomainRoot; ?>portfolio" title="Explore design work completed by the designer behind <?= $globalTitle; ?>" hreflang="en">
+                <img loading="lazy" src="/dist/images/work/digital-design-subscription-01.jpg" alt="Unlimited design subscriptions work by Drip Fed Design agency" class="__image" width="900px" height="550px" />
+            </a>
         </div>
         <div class="__slide __two">
-            <img src="/dist/images/work/digital-design-subscription-02.jpg" alt="Unlimited design subscriptions work by Drip Fed Design agency" class="__image" width="900px" height="550px" />
+            <a data-cabin-event="Portfolio / IMG-01" href="<?= $globalDomainRoot; ?>portfolio" title="Explore design work completed by the designer behind <?= $globalTitle; ?>" hreflang="en">
+                <img src="/dist/images/work/digital-design-subscription-02.jpg" alt="Unlimited design subscriptions work by Drip Fed Design agency" class="__image" width="900px" height="550px" />
+            </a>
         </div>
         <div class="__slide __button">
             <a data-cabin-event="Portfolio / More Work" href="<?= $globalDomainRoot; ?>portfolio" title="Explore design work completed by the designer behind <?= $globalTitle; ?>" hreflang="en">
@@ -12,13 +16,19 @@
             </a>
         </div>
         <div class="__slide __three">
-            <img loading="lazy" src="/dist/images/work/digital-design-subscription-03.jpg" alt="Unlimited design subscriptions work by Drip Fed Design agency" class="__image" width="900px" height="550px" />
+            <a data-cabin-event="Portfolio / IMG-01" href="<?= $globalDomainRoot; ?>portfolio" title="Explore design work completed by the designer behind <?= $globalTitle; ?>" hreflang="en">
+                <img loading="lazy" src="/dist/images/work/digital-design-subscription-03.jpg" alt="Unlimited design subscriptions work by Drip Fed Design agency" class="__image" width="900px" height="550px" />
+            </a>
         </div>
         <div class="__slide __four">
-            <img loading="lazy" src="/dist/images/work/digital-design-subscription-04.jpg" alt="Unlimited design subscriptions work by Drip Fed Design agency" class="__image" width="900px" height="550px" />
+            <a data-cabin-event="Portfolio / IMG-01" href="<?= $globalDomainRoot; ?>portfolio" title="Explore design work completed by the designer behind <?= $globalTitle; ?>" hreflang="en">
+                <img loading="lazy" src="/dist/images/work/digital-design-subscription-04.jpg" alt="Unlimited design subscriptions work by Drip Fed Design agency" class="__image" width="900px" height="550px" />
+            </a>
         </div>
         <div class="__slide __five">
-            <img loading="lazy" src="/dist/images/work/digital-design-subscription-05.jpg" alt="Unlimited design subscriptions work by Drip Fed Design agency" class="__image" width="900px" height="550px" />
+            <a data-cabin-event="Portfolio / IMG-01" href="<?= $globalDomainRoot; ?>portfolio" title="Explore design work completed by the designer behind <?= $globalTitle; ?>" hreflang="en">
+                <img loading="lazy" src="/dist/images/work/digital-design-subscription-05.jpg" alt="Unlimited design subscriptions work by Drip Fed Design agency" class="__image" width="900px" height="550px" />
+            </a>
         </div>
     </div>
 </section>
